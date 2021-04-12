d973cd0e56867df560ea008e6d93051dc5240d5a
Gregory K <grigory.koltashev@gmail.com>
2021-04-12 21:22:41 +0300

    Database connection creds set up

--- login.php
+++ login.php
@@ -1,11 +1,16 @@
 <?PHP
 	//establish connection to MySQL and access the eternal DB
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "eternal";
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
+
+	$active_group = 'default';
+	$query_builder = TRUE;
 	
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+	//$conn = new mysqli($servername, $username, $password, $dbname);
 	
 	//provisional clause for output in case of connection failure
 	if ($conn->connect_error) {
